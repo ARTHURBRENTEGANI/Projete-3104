959e07368ee93f171ffd1aaac365be61ed8364e3
Arthur Brentegani <146497039+ARTHURBRENTEGANI@users.noreply.github.com>
2024-09-13 12:38:46 -0300

    espservidorteste.ino

--- espservidorteste.ino
+++ espservidorteste.ino
@@ -12,106 +12,100 @@ WiFiServer server(600);
 
 void setup() 
 {
-Serial.begin(115200);
+  Serial.begin(115200);
 
-if(WiFi.softAP(nome,senha))
-{
-Serial.println("rede Wifi gerada com sucesso !!!!");
-Serial.println("o ip do servidor do esp servidor é :");
-Serial.println(WiFi.softAPIP());
-// o ip do esp é 192.168.4.1
-}
-else
-{
-Serial.println("erro ao gerar a rede wifi");  
-}
+  if (WiFi.softAP(nome, senha))
+  {
+    Serial.println("Rede WiFi gerada com sucesso !!!!");
+    Serial.println("O IP do servidor do ESP é:");
+    Serial.println(WiFi.softAPIP());
+    // O IP do ESP é 192.168.4.1
+  }
+  else
+  {
+    Serial.println("Erro ao gerar a rede WiFi");  
+  }
 
-server.begin();
+  server.begin();
 
-pinMode(ledvermelho, OUTPUT);
-pinMode(ledverde, OUTPUT);
-pinMode(ledamarelo, OUTPUT);
+  pinMode(ledvermelho, OUTPUT);
+  pinMode(ledverde, OUTPUT);
+  pinMode(ledamarelo, OUTPUT);
 
-digitalWrite(ledverde,0);
-digitalWrite(ledvermelho,0);
-digitalWrite(ledamarelo,0);
+  digitalWrite(ledverde, LOW);
+  digitalWrite(ledvermelho, LOW);
+  digitalWrite(ledamarelo, LOW);
 }
 
 void loop()
 {
+  WiFiClient client = server.available(); // Verifica se há um cliente tentando conectar
 
- WiFiClient client = server.available();// a função server.available() verifica se tem um cliente tentando conectar e se tiver retorna um valor WiFiClient e se não estiver um cliente 
- //tentando se conectar retorna um WiFiClient inválido, se a função server.available() retorna um objeto válido a variavel client tera umvalor válido 
- 
-if (client != WiFiClient()) // verifica se avariavel client não é inválida
-{
-Serial.println("Existe um clliente conectado com sucesso");
-
-if(client.connected()) //enquanto o cliente estiver conectado
- {
-if(client.available()) // enquanto o cliente estiver mandando alguma coisa
+  if (client)
   {
-resposta_cliente = client.readStringUntil('\r'); // le a resposta enviada pelo cliente
-if(resposta_cliente.length() > 0)
+    Serial.println("Cliente conectado com sucesso");
+
+    if (client.connected())
     {
-Serial.println("Cliente mandou alguma coisa: ");
-Serial.println(resposta_cliente);
-if (resposta_cliente.startsWith("carbono:"))
-{
+      if (client.available())
+      {
+        resposta_cliente = client.readStringUntil('\r'); // Lê a resposta enviada pelo cliente
+        if (resposta_cliente.length() > 0)
+        {
+          Serial.println("Cliente mandou alguma coisa:");
+          Serial.println(resposta_cliente);
 
-int carbono_analog = resposta_cliente.substring(8,resposta_cliente.length()-1).toInt();
-Serial.println("o valor do sensnro de carbono chedao no servidor é: ");
-Serial.println(carbono_analog);
-if(carbono_analog > 40)
-{
-Serial.println("acendeu led carbono");
-digitalWrite(ledvermelho,1);
-}
- else
-{
-digitalWrite(ledvermelho,0);
-Serial.println("apagou led carbono");
-}
-
-}
-else if(resposta_cliente.startsWith("microfone:"))
-{
-int microfone_analog =resposta_cliente.substring(10,resposta_cliente.length() - 1).toInt(); 
-Serial.println("o valor do microfone que chegou no servidor é: ");
-Serial.println(microfone_analog);
-if(microfone_analog > 200)
-{
-
-Serial.println("acendeu led microfone");
-digitalWrite(ledverde,1);
-}
-else
-{
-digitalWrite(ledverde,0);
-Serial.println("apagou led microfone");
-}
-}
-client.stop(); // desconecta o cliente
-Serial.println("Cliente desconectado");
-
-}
-else
-{
-bool x;
-while(x == 1)
-{
-Serial.println("a resposta do cliente esta vazia código esta travado dentro do bloco while");
-}
-
-}
+          if (resposta_cliente.startsWith("carbono:"))
+          {
+            int carbono_analog = resposta_cliente.substring(8, resposta_cliente.length() - 1).toInt();
+            Serial.println("Valor do sensor de carbono recebido no servidor:");
+            Serial.println(carbono_analog);
+            if (carbono_analog > 40)
+            {
+              Serial.println("Acendeu LED de carbono");
+              digitalWrite(ledvermelho, HIGH);
+            }
+            else
+            {
+              digitalWrite(ledvermelho, LOW);
+              Serial.println("Apagou LED de carbono");
+            }
+          }
+          else if (resposta_cliente.startsWith("microfone:"))
+          {
+            int microfone_analog = resposta_cliente.substring(10, resposta_cliente.length() - 1).toInt();
+            Serial.println("Valor do microfone recebido no servidor:");
+            Serial.println(microfone_analog);
+            if (microfone_analog > 200)
+            {
+              Serial.println("Acendeu LED do microfone");
+              digitalWrite(ledverde, HIGH);
+            }
+            else
+            {
+              digitalWrite(ledverde, LOW);
+              Serial.println("Apagou LED do microfone");
+            }
+          }
+          else if (resposta_cliente.startsWith("pessoa_errada"))
+          {
+            Serial.println("Classe person_wrong detectada");
+            digitalWrite(ledamarelo, HIGH); // Acende o LED amarelo
+          }
+          else
+          {
+            digitalWrite(ledamarelo, LOW); // Apaga o LED amarelo se não for a classe esperada
+          }
+          client.stop(); // Desconecta o cliente
+          Serial.println("Cliente desconectado");
+        }
+      }
+    }
   }
-
- }
-}
-else
-{
-Serial.println("não tem nehum cliente tentando se conectar");
-}
-delay(5000);
-resposta_cliente = " ";
+  else
+  {
+    Serial.println("Nenhum cliente tentando se conectar");
+  }
+  delay(5000);
+  resposta_cliente = " ";
 }
